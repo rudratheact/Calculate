192550e892c6ec0fe9d24c0025295e5948f2a87f
RUDRA S M <45320438+rudratheact@users.noreply.github.com>
2020-09-01 23:57:40 +0530

    Add files via upload

---
+++ Calculate.framework/Headers/Calculate.h
@@ -0,0 +1,19 @@
+//
+//  Calculate.h
+//  Calculate
+//
+//  Created by Trainer on 01/09/20.
+//  Copyright © 2020 training. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calculate.
+FOUNDATION_EXPORT double CalculateVersionNumber;
+
+//! Project version string for Calculate.
+FOUNDATION_EXPORT const unsigned char CalculateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calculate/PublicHeader.h>
+
+
